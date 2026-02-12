e50f07a68e26e683a54a041b63925e3c35847bd8
MagicJester2764 <30580575+MagicJester2764@users.noreply.github.com>
2026-02-12 10:13:04 -0500

    Update models.rs
    AbuseReport and ResolveReportRequest are designed weirdly with async, so they make rustc think they are dead code. They are not.

--- src/models.rs
+++ src/models.rs
@@ -161,6 +161,7 @@ pub struct SubmitReportRequest {
 
 /// Abuse report details
 #[derive(Serialize)]
+#[allow(dead_code)]
 pub struct AbuseReport {
     pub id: i64,
     pub short_code: String,
@@ -179,6 +180,7 @@ pub struct AbuseReport {
 
 /// Request to resolve an abuse report
 #[derive(Serialize, Deserialize)]
+#[allow(dead_code)]
 pub struct ResolveReportRequest {
     pub action: String, // "dismiss", "delete_url", "ban_user"
 }
